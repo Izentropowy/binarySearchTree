0ee2a3f9df186cde235d0ed03cb1b069520805aa
Izentropowy <119621854+Izentropowy@users.noreply.github.com>
2024-02-13 23:21:42 +0100

    Add insert function to bst

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
-const Node = () => {
+const Node = (data) => {
   return {
-    data: null,
+    data: data,
     left: null,
     right: null,
   };
 };
 
-const Tree = (arr) => {
+const Tree = (treeArr) => {
   // sort and remove duplicates
-  arr = arr.sort();
-  arr = [...new Set(arr)];
+  treeArr = treeArr.sort();
+  treeArr = [...new Set(treeArr)];
 
-  let start = arr[0];
-  let end = arr[arr.length - 1];
+  let treeStart = 0;
+  let treeEnd = treeArr.length - 1;
+
+  let root = buildTree(treeArr, treeStart, treeEnd);
+
+  function prettyPrint(node = root, prefix = "", isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  }
+
   function buildTree(arr, start, end) {
     if (start > end) return null;
-    let mid = (start + end) / 2;
-    let root = Node();
-    root.data = arr[mid];
+    let mid = parseInt((start + end) / 2);
+    let root = Node(arr[mid]);
 
     root.left = buildTree(arr, start, mid - 1);
     root.right = buildTree(arr, mid + 1, end);
@@ -25,8 +40,36 @@ const Tree = (arr) => {
     return root;
   }
 
-  console.log(buildTree(arr, start, end));
+  function insert(val) {
+    let currentNode = root;
+    let previousNode = null;
+    while (currentNode) {
+      if (currentNode.data === val) return;
+      if (currentNode.data > val) {
+        previousNode = currentNode;
+        currentNode = currentNode.left;
+      } else {
+        previousNode = currentNode;
+        currentNode = currentNode.right;
+      }
+    }
+
+    if (previousNode.data > val) previousNode.left = Node(val);
+    else previousNode.right = Node(val);
+  }
+
+  return {
+    prettyPrint,
+    insert,
+  };
 };
 
 let arr = [1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7];
 let newTree = Tree(arr);
+newTree.prettyPrint();
+newTree.insert(9);
+newTree.prettyPrint();
+newTree.insert(8);
+newTree.prettyPrint();
+newTree.insert(7.5);
+newTree.prettyPrint();
